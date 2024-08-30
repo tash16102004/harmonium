da25c27372a1f477f53dd71ab544d12fc78df802
tash16102004 <112432309+tash16102004@users.noreply.github.com>
2024-08-31 00:54:33 +0530

    Create service worker.js

---
+++ service worker.js
@@ -0,0 +1,22 @@
+const staticDevCoffee = "web-harmonium";
+const assets = [
+  "/",
+  "/webharmonium.html",
+  "/harmonium-kannan-orig.wav"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
